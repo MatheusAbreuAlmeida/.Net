8cd75b2ce8b81f5b5e6e83f9380d915f6b92f343
Matheus Abreu de Almeida <31114355+MatheusAbreuAlmeida@users.noreply.github.com>
2020-02-04 17:24:07 -0300

    CosmosAPI
    .NET webAPI

---
+++ Cosmos.API/Controllers/ControleItem.cs
@@ -0,0 +1,61 @@
+﻿using Cosmos.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cosmos.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ControleItem : ControllerBase
+    {
+        private readonly IDocumentClient _documentClient;
+        readonly string databaseID;
+        readonly string collectionID;
+        public IConfiguration Configuration { get; }
+
+        public ControleItem(IDocumentClient documentClient, IConfiguration configuration)
+        {
+            _documentClient = documentClient;
+            Configuration = configuration;
+
+            databaseID = Configuration["DatabaseID"];
+            collectionID = "Itens";
+
+            BuildCollection().Wait();
+        }
+
+        private async Task BuildCollection()
+        {
+            await _documentClient.CreateDatabaseIfNotExistsAsync(new Database { Id = databaseID });
+            await _documentClient.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri(databaseID),
+                new DocumentCollection { Id = collectionID });
+        }
+        [HttpGet]
+        public IQueryable<Item> Get()
+        {
+            return _documentClient.CreateDocumentQuery<Item>(UriFactory.CreateDocumentCollectionUri(databaseID, collectionID), new FeedOptions { MaxItemCount = 20 });
+
+        }
+
+        [HttpGet("{Id}")]
+        public IQueryable<Item> Get(string ID)
+        {
+            return _documentClient.CreateDocumentQuery<Item>(UriFactory.CreateDocumentCollectionUri(databaseID,collectionID),
+                new FeedOptions { MaxItemCount = 1}).Where((i) => i.Id == ID);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Item item)
+        {
+            var response = await _documentClient.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(databaseID, collectionID), item);
+            return Ok();
+        }
+    }
+}
---
+++ Cosmos.API/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cosmos.API.Models
+{
+    public class Item 
+    {
+        [Newtonsoft.Json.JsonProperty(PropertyName = "Id")]
+        public string Id { get; set; }
+        
+        [Newtonsoft.Json.JsonProperty(PropertyName = "Nome")]
+        public string Nome { get; set; }
+        
+        [Newtonsoft.Json.JsonProperty(PropertyName = "Descricao")]
+        public string Descricao { get; set; }
+    }
+    
+  
+}
